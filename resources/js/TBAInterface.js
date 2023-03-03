d933f1e2077ed24b55d0ad5477c0abc1edddfd8a
sawyer-thompson <107327415+sawyer-thompson@users.noreply.github.com>
2023-03-03 12:37:42 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "1dUoHX7L5MXHFoAsO3qBw5K5p9A03vqPd339P3lPwnAqqjghMI9iFDFP3Rz3sVQH";
 /**
  * Get list of teams in event
  *
